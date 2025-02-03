fc57e09d1b909e9482f5caca328129b249475e93
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-03 22:36:06 +0600

    Update

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 
 def index(request):
     if request.method == 'POST':
-        city = request.POST['city']
+        country = request.POST.get('country', '')
+        city = request.POST.get('city', '')
         api_key = 'your_api_key'
-        api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+        api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric'
         response = requests.get(api_url)
         data = response.json()
         context = {
+            'country': country,
             'city': city,
             'data': data,
         }
