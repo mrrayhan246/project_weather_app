ba5238e3f3652bfc4fb9bc7524e9cd1cf9aae46a
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-03 22:24:53 +0600

    Update

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -1,3 +1,16 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        api_key = 'your_api_key'
+        api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+        response = requests.get(api_url)
+        data = response.json()
+        context = {
+            'city': city,
+            'data': data,
+        }
+        return render(request, 'weather/index.html', context)
+    return render(request, 'weather/index.html')
