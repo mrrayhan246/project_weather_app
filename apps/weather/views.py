7c63096635e08a477fd204daa0cfce862b1701b0
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-03 22:46:56 +0600

    Update

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -5,7 +5,7 @@ def index(request):
     if request.method == 'POST':
         country = request.POST.get('country', '')
         city = request.POST.get('city', '')
-        api_key = 'your_api_key'
+        api_key = '9946c752ce229e1853a1a7be7efdca02'
         api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric'
         response = requests.get(api_url)
         data = response.json()
